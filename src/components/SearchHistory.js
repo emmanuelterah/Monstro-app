d2cde022f24ab268e091abf931a128bf3025a873
emmanuelterah <emmanuelterah98@gmail.com>
2024-02-15 13:37:57 +0300

    Ch

--- src/components/SearchHistory.js
+++ src/components/SearchHistory.js
@@ -6,7 +6,7 @@ const SearchHistory = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:3000/cities');
+        const response = await fetch('http://localhost:3001/cities');
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
@@ -33,3 +33,4 @@ const SearchHistory = () => {
 };
 
 export default SearchHistory;
+
