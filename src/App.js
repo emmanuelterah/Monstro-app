bfbf5f2cc9ca451ce76d4782b6fdf8625ccbe349
Edith <edith.chelangat@student.moringaschool.com>
2024-02-15 16:55:00 +0300

    Modify app js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
-import React, { useState } from 'react';
-import BackgroundDisplay from './components/BackgroundDisplay';
-import WeatherCard from './components/WeatherCard';
-import WeatherForm from './components/WeatherForm';
-import SearchHistory from './components/SearchHistory';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './components/Navbar'; // Corrected import statement
+
 import TemperatureConverter from './components/TemperatureConverter';
 
-const App = () => {
-  const [selectedCity, setSelectedCity] = useState(null);
-
-  const handleSubmit = async (values, actions) => {
-    try {
-      // Replace this URL with your actual weather data API
-      const response = await fetch(`http://localhost:8001/weather?city=${values.city}&country=${values.country}`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch weather data');
-      }
-
-  const data = await response.json();
-  setSelectedCity(data);
-} catch (error) {
-  console.error('Error fetching weather data:', error);
-} finally {
-  actions.resetForm();
-}
-  };
-
+function App() {
   return (
-    <div className="App">
-       <BackgroundDisplay selectedCity={selectedCity} />
-      <WeatherForm handleSubmit={handleSubmit} />
-      <WeatherCard selectedCity={selectedCity} />
-      <SearchHistory />
-      <TemperatureConverter />
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Route path="/TemperatureConverter" component={TemperatureConverter} />
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;
-
