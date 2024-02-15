4836b55c3f0defe628bfd65b0c196750ec7ceaa1
emmanuelterah <emmanuelterah98@gmail.com>
2024-02-15 19:16:48 +0300

    latest

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TemperatureConverter from './components/TemperatureConverter';
 import About from './components/About';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
-import {Router, Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 
 const App = () => {
   const [selectedCity, setSelectedCity] = useState(null);
@@ -28,6 +28,29 @@ const App = () => {
 }
   };
 
+  const handleSubmissions = async (values, setSubmitting, resetForm) => {
+    try {
+      const response = await fetch('http://localhost:8001/cities', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+  
+  if (!response.ok) {
+    throw new Error(`Failed to submit data: ${response.status} - ${response.statusText}`);
+  }
+  
+  resetForm();
+  console.log('Data submitted successfully:', values);
+    } catch (error) {
+      console.error('Error submitting data:', error);
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
   return (
     <div className="App">
       <BackgroundDisplay selectedCity={selectedCity} />
@@ -38,7 +61,7 @@ const App = () => {
         <Route path='/TemperatureConverter' element={<TemperatureConverter/>}/>
       </Routes>
       {/* <WeatherCard selectedCity={selectedCity} /> */}
-      <WeatherForm handleSubmit={handleSubmit} />
+      <WeatherForm handleSubmit={handleSubmit} handleSubmissions={handleSubmissions} />
      
       <SearchHistory />
     </div>
