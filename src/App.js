e551601ef15dd1cfe5c6ee6d5aab35eaa168fc36
Enoch Kibet <enochblake9@gmail.com>
2024-02-15 13:44:56 +0300

    db.json and WeatherCard.js and about.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import BackgroundDisplay from './BackgroundDisplay';
+import WeatherCard from './WeatherCard';
+import WeatherForm from './WeatherForm';
+import SearchHistory from './SearchHistory';
+import TemperatureConverter from './TemperatureConverter';
 
-function App() {
+const App = () => {
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  const handleSubmit = async (values, actions) => {
+    try {
+      const response = await fetch(`http://localhost:8001/weather?city=${values.city}&country=${values.country}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch weather data');
+      }
+
+      const data = await response.json();
+      setSelectedCity(data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    } finally {
+      actions.resetForm();
+    }
+  };
+
   return (
-    <div className="App">
-      
+    <div>
+
+      <BackgroundDisplay selectedCity={selectedCity} />
+
+      <WeatherForm handleSubmit={handleSubmit} />
+
+      <WeatherCard selectedCity={selectedCity} />
+
+      <SearchHistory />
+
+      <TemperatureConverter />
     </div>
   );
-}
+};
 
 export default App;
