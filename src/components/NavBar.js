d735c0ae10e155d7c715560149531891fa975fd5
emmanuelterah <emmanuelterah98@gmail.com>
2024-02-15 21:24:05 +0300

    latest changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function NavBar() {
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/About">About</NavLink>
-      <NavLink to="/TemperatureConverter">TemperatureConverter</NavLink>
+    <div className="navbar-container">
+      <FontAwesomeIcon icon={faCoffee} className="coffee-icon" />
+      <ul>
+      <NavLink className="samoa" to="/">Home</NavLink>
+      <NavLink className="samoa" to="/About">About</NavLink>
+      <NavLink className="samoa" to="/TemperatureConverter">TemperatureConverter</NavLink>
+      </ul>
+
+    <img src="" className="darkMode"></img>
     </div>
   );
 }
