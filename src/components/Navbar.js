171b242760c6268cbfdbbb781a35b8d43fb08091
Edith <edith.chelangat@student.moringaschool.com>
2024-02-17 12:42:11 +0300

    Add functionalities

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import  { faCloudMoon } from "@fortawesome/free-solid-svg-icons";
+import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
 
 
-function Navbar() {
+function NavBar() {
   return (
-    <div className='navbar'>
-      <Link to="/" className="nav-link">Home</Link>
-      <Link to="/about" className="nav-link">About</Link>
-      <Link to="/weather-form" className="nav-link">Weather Form</Link>
+    <div className="navbar-container">
+      <FontAwesomeIcon icon={faCloudSunRain} className="moon-icon" />
+      <ul>
+      <NavLink className="samoa" to="/">Home</NavLink>
+      <NavLink className="samoa" to="/About">About</NavLink>
+      <NavLink className="samoa" to="/TemperatureConverter">TemperatureConverter</NavLink>
+      <NavLink className="samoa" to="/WeatherForm">WeatherForm</NavLink>
+      <NavLink className="samoa" to="/WeatherComparison">Weather Comparison</NavLink>
+      </ul>
+    <FontAwesomeIcon icon={faCloudMoon} className="moon-icon"/>
     </div>
   );
 }
 
-export default Navbar;
+export default NavBar;
