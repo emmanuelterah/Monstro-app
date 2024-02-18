880caedc9a03f8ebeb96b04b06bc2d576b42a17d
emmanuelterah <emmanuelterah98@gmail.com>
2024-02-18 14:39:57 +0300

    Code cleanup and unnecessary comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
 import BackgroundDisplay from './components/BackgroundDisplay';
-// import WeatherCard from './components/WeatherCard';
-// import WeatherForm from './components/WeatherForm';
-// import SearchHistory from './components/SearchHistory';
 import TemperatureConverter from './components/TemperatureConverter';
 import About from './components/About';
 import NavBar from './components/NavBar'
